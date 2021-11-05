5b64e39a0a672e76fee157694dee161f43ebe0e3
haran2001 <hari.ayapps@gmail.com>
2021-11-06 00:27:22 +0530

    indentation corrections

--- Q27.c
+++ Q27.c
@@ -14,25 +14,30 @@ int main(int arvuments, char* arv[])
     pid_t childid;
     char readBuff[size];
     pipe(file);
+    
     if( arvuments != 3 ) 
     {
         printf("ERROR: Need exactly 2 parameters.\n");
         exit(1);
     }
+    
     int fileOpen = open(arv[1], 0);
     int TargetFile = open(arv[2], O_RDWR|O_CREAT|O_APPEND, 0666);
+    
     if (fileOpen == -1 || TargetFile == -1) 
     {
-        printf("File can not be opend\n");
+        printf("File can not be opened\n");
         exit(1);
     }
+    
     childid = fork();
+    
     if (childid == 0) 
     {
         close(file[1]);
-	while (read(file[0], readBuff, sizeof(readBuff)) > 0) 
-	{
-		write(TargetFile, readBuff, strlen(readBuff) - 1);
+        while (read(file[0], readBuff, sizeof(readBuff)) > 0)
+	    {
+		    write(TargetFile, readBuff, strlen(readBuff) - 1);
         }
         close(file[0]);
         close(TargetFile);
@@ -40,8 +45,8 @@ int main(int arvuments, char* arv[])
     else 
     {
         close(file[0]);
-	while (read(fileOpen, readBuff, sizeof(readBuff)) > 0) 
-	{
+	    while (read(fileOpen, readBuff, sizeof(readBuff)) > 0) 
+	    {
             write(file[1], readBuff, sizeof(readBuff));
             memset(readBuff, 0, size);
         }
