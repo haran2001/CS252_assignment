9677c1d323c00c36b6759bd8e81c48b0a0d97bcd
haran2001 <hari.ayapps@gmail.com>
2021-11-12 16:37:45 +0530

    tab to space fix

--- Q21.c
+++ Q21.c
@@ -4,50 +4,50 @@
 
 int main()
 {
-	//Declaration and initialization of variables
-	int n=0;
-	int k=0; 
-	pid_t pid;
+    //Declaration and initialization of variables
+    int n=0;
+    int k=0; 
+    pid_t pid;
 
-	//Check if number entered is strictly greater than zero.
-	//If not enter new number
-	do
-	{
-		printf("Please enter a number greater than 0 to run the Collatz Conjecture.\n"); 
-		scanf("%d", &k);	
-	} while (k <= 0);
+    //Check if number entered is strictly greater than zero.
+    //If not enter new number
+    do
+    {
+    	printf("Please enter a number greater than 0 to run the Collatz Conjecture.\n"); 
+    	scanf("%d", &k);	
+    } while (k <= 0);
 
-	//Fork system call for parent-child relationship 
-	pid = fork();
+    //Fork system call for parent-child relationship 
+    pid = fork();
 
-	//Child Implements the Collatz Algorithm
-	if (pid == 0)
-	{
-		printf("Child is working...\n");
-		printf("%d\n",k);
-		while (k!=1)
-		{
-			if (k%2 == 0)
-			{
-				k = k/2;
-			}
-			else if (k%2 == 1)
-			{
-				k = 3 * (k) + 1;
-			}	
-		
-			printf("%d\n",k);
-		}
+    //Child Implements the Collatz Algorithm
+    if (pid == 0)
+    {
+    	printf("Child is working...\n");
+    	printf("%d\n",k);
+    	while (k!=1)
+    	{
+    		if (k%2 == 0)
+    		{
+    			k = k/2;
+    		}
+    		else if (k%2 == 1)
+    		{
+    			k = 3 * (k) + 1;
+    		}	
+    	
+    		printf("%d\n",k);
+    	}
 
-		printf("Child process is done.\n");
-	}
+    	printf("Child process is done.\n");
+    }
 
-	//Parent starts the child process and waits.
-	else
-	{
-		printf("Parents is waiting on child process...\n");
-		wait();
-		printf("Parent process is done.\n");
-	}
-	return 0; 
+    //Parent starts the child process and waits.
+    else
+    {
+    	printf("Parents is waiting on child process...\n");
+    	wait();
+    	printf("Parent process is done.\n");
+    }
+    return 0; 
 }
